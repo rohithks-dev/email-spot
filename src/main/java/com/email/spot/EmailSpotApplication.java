17596c75cbfbae44d756cc364cf4be078b8409e1
Rohithks <surarohith97@gmail.com>
2024-04-23 17:10:53 -0400

    Initial Commit with PostgreSQL connection
    Initial Commit with PostgreSQL connection

---
+++ src/main/java/com/email/spot/EmailSpotApplication.java
@@ -0,0 +1,13 @@
+package com.email.spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email/spot/EmailSpotApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.email.spot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
